efd18bf5aa1c141d103ac24ab5ccd0577d4f9370
kylebell8 <kyle.bell2001@hotmail.com>
2023-08-25 01:31:46 +0100

    test

---
+++ test.py
@@ -0,0 +1,10 @@
+#make small piece of code repeating numbers 1 - 10
+
+for i in range(1,11):
+    print(i)
+
+#make small piece of code repeating numbers 1 - 10 in reverse order
+
+for i in range(10,0,-1):
+    print(i)
+    
