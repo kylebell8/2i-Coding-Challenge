8d431beb12d62c9cebc2811d3a546011c490eb1e
kylebell8 <kyle.bell2001@hotmail.com>
2023-08-25 20:52:05 +0100

    test completed at a basic level, example arrays + number pairs used for testing output

--- App.py
+++ App.py
@@ -1,4 +1,27 @@
-#25/08/2023
-#Description: 2i Coding Challenge - Test
-#Name: Kyle Bell
+def find_pairs(nums, X):
+    """
+    :param nums: List[int] - A sorted array of whole numbers
+    :param X: int - The target sum
+    :return: int - The count of pairs that sum to X
+    """
+    count = 0
+    i, j = 0, len(nums) - 1
+    
+    while i < j:
+        current_sum = nums[i] + nums[j]
+        
+        if current_sum == X:
+            count += 1
+            i += 1
+            j -= 1
+        elif current_sum < X:
+            i += 1
+        else:
+            j -= 1
+    
+    return count
 
+# Test the function
+print(find_pairs([3, 4, 5, 6], 1))  # Output should be 0
+print(find_pairs([0, 15, 32, 2000, 15000], 15))  # Output should be 1
+print(find_pairs([1, 1, 10, 32, 41], 42))  # Output should be 2
